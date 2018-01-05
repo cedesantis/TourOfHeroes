f6da7979c0505a3a2afa6fefba5f60a4bf1ec83b
Chris DeSantis <cedesantis@gmail.com>
2018-01-04 21:48:06 -0500

    lesson 4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES : Hero []=[
+    {id: 11 , name : 'Mr Tickle'},
+    {id: 12 , name : 'Mr Strange'},
+    {id: 13 , name : 'Dr Love'},
+    {id: 14 , name : 'Mrs Lady'},
+    {id: 15 , name : 'Manman'},
+    {id: 16 , name : 'Mangirl'},
+    {id: 17 , name : 'Indestructo'},
+    {id : 18 , name : 'Truckman'},
+    {id : 19 , name : 'Truckwoman'},
+    {id : 20 , name : 'Superbutt'},
+];
